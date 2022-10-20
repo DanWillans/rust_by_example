152b1821ff19193314e7f0ee0d9cfe89b117112d
Dan Willans <willansdan@googlemail.com>
2022-10-20 22:55:42 +0100

    Add Section 8 - Control Flow - Part 2

--- src/flow_of_control/destructing.rs
+++ src/flow_of_control/destructing.rs
@@ -1,4 +1,4 @@
-pub fn destructing() {
+fn destructing_tuples(){
     let triple = (0, -2, 3);
     // TODO ^ Try different values for `triple`
 
@@ -14,4 +14,142 @@ pub fn destructing() {
         _ => println!("It doesn't matter what they are"),
         // `_` means don't bind the value to a variable
     }
+}
+
+fn destructing_array_slices() {
+    // Try changing the values in the array, or make it a slice!
+    let array = [1, -2, 6];
+
+    match array {
+        // Binds the second and the third elements to the respective variables
+        [0, second, third] =>
+            println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+
+        // Single values can be ignored with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // The code below would not compile
+        // [-1, second] => ...
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+}
+
+// `allow` required to silence warnings because only
+// one variant is used.
+#[allow(dead_code)]
+enum Color {
+    // These 3 are specified solely by their name.
+    Red,
+    Blue,
+    Green,
+    // These likewise tie `u32` tuples to different names: color models.
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+fn destructing_enums() {
+    let color = Color::RGB(122, 17, 40);
+    // TODO ^ Try different variants for `color`
+
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red   => println!("The color is Red!"),
+        Color::Blue  => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) =>
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+                c, m, y, k),
+        // Don't need another arm because all variants have been examined
+    }
+}
+
+fn destructing_pointers_and_ref() {
+    // Assign a reference of type `i32`. The `&` signifies there
+    // is a reference being assigned.
+    let reference = &4;
+
+    match reference {
+        // If `reference` is pattern matched against `&val`, it results
+        // in a comparison like:
+        // `&i32`
+        // `&val`
+        // ^ We see that if the matching `&`s are dropped, then the `i32`
+        // should be assigned to `val`.
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    // To avoid the `&`, you dereference before matching.
+    match *reference {
+        val => println!("Got a value via dereferencing: {:?}", val),
+    }
+
+    // What if you don't start with a reference? `reference` was a `&`
+    // because the right side was already a reference. This is not
+    // a reference because the right side is not one.
+    let _not_a_reference = 3;
+
+    // Rust provides `ref` for exactly this purpose. It modifies the
+    // assignment so that a reference is created for the element; this
+    // reference is assigned.
+    let ref _is_a_reference = 3;
+
+    // Accordingly, by defining 2 values without references, references
+    // can be retrieved via `ref` and `ref mut`.
+    let value = 5;
+    let mut mut_value = 6;
+
+    // Use `ref` keyword to create a reference.
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    // Use `ref mut` similarly.
+    match mut_value {
+        ref mut m => {
+            // Got a reference. Gotta dereference it before we can
+            // add anything to it.
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m);
+        },
+    }
+}
+
+pub fn destructing() {
+    destructing_tuples();
+    destructing_array_slices();
+    destructing_enums();
+    destructing_pointers_and_ref();
 }
--- src/flow_of_control/mod.rs
+++ src/flow_of_control/mod.rs
@@ -3,6 +3,7 @@ pub mod loop_control;
 pub mod while_control;
 pub mod for_and_range;
 pub mod match_control;
+pub mod destructing;
 
 pub fn call_all(){
   if_else::if_else();
@@ -10,4 +11,5 @@ pub fn call_all(){
   while_control::while_control();
   for_and_range::for_and_range();
   match_control::match_control();
+  destructing::destructing();
 }
