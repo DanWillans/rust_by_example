cb11a0aed58224e3bbf43f893b5d7869791a8c27
Dan Willans <dwillans@amazon.com>
2022-10-31 13:10:49 +0000

    Add Section 9 - Functions

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -1,5 +1,13 @@
 pub mod functions;
+pub mod associated_functions;
+pub mod closures;
+pub mod higher_order_functions;
+pub mod diverging_functions;
 
 pub fn call_all(){
   functions::functions();
+  associated_functions::associated_functions();
+  closures::closures();
+  higher_order_functions::higher_order_functions();
+  diverging_functions::diverging_functions();
 }
